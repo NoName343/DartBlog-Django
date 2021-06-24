414a01bdbcc5cff09773746314c3edf6a0ba44a1
Pasha <admin@supper.com>
2021-06-24 15:48:57 +0300

    add popular post

--- Blog/admin.py
+++ Blog/admin.py
@@ -20,10 +20,10 @@ class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
     save_as = True
     save_on_top = True
-    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
+    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo', 'views')
     list_display_links = ('id', 'title')
     search_fields = ('title',)
-    list_filter = ('category',)
+    list_filter = ('category', 'tags')
 
     def get_photo(self, obj):
         if obj.photo:
--- Blog/views.py
+++ Blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
+from django.db.models import F
 
 class Home(ListView):
     model = Post
     template_name = 'Blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -16,7 +17,7 @@ class Home(ListView):
 class PostsByCategory(ListView):
     template_name = 'Blog/get_post_category.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 4
     allow_empty = False # 404
 
     def get_queryset(self):
@@ -28,5 +29,28 @@ class PostsByCategory(ListView):
         return context
 
 
-def get_post(request, slug):
-    return render(request, 'Blog/category.html')
+class GetPost(DetailView):
+    model = Post
+    template_name = 'Blog/single.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
+
+class GetTag(ListView):
+    template_name = 'Blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
